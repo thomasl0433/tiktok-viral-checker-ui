193b0054b0fc2a47d4c8b0af87b9f15439194b54
thomasl0433 <61367229+thomasl0433@users.noreply.github.com>
2022-03-30 14:32:47 -0400

    frontend fixes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ class App extends React.Component {
   state = {
     term: "",
     hasSearched: false,
-    isViral: false,
+    isViral: null,
     hasSubmitted: false,
     data: {},
+    song: {}
   };
 
   onFormSubmit = async (event) => {
@@ -16,72 +17,94 @@ class App extends React.Component {
     this.setState({
       hasSearched: true,
       hasSubmitted: true,
+      data: {},
     });
 
     // shoot off request to backend
     //console.log(`Hitting http://localhost:3001/${this.state.term}}`);
-    const response = await fetch(`http://localhost:3001/${this.state.term}`);
+    const response = await fetch(`http://localhost:3001/widesearch/${this.state.term}`);
 
     const output = await response.json();
     try {
       this.setState({
         isViral: output.viral,
-        data: output
+        data: output,
       });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-    
+
     console.log(output);
   };
 
   renderSearch = () => {
+    let id = 0;
     console.log("inside renderSearch");
-    const items = this.state.data.map(item =>
-      <li key={item.name}>{item.name}</li>
-    )
+    const items = this.state.data.map((item) => (
+      <button onClick={() => this.onSongSelect(item)} className="btn btn-light rounded w-50 mx-auto d-block mb-2" key={id++}>
+        {item.name} by {item.artist}
+      </button>
+    ));
 
     return items;
   };
 
+  onSongSelect = async (item) => {
+    // this will determine if the song is viral or not (return TRUE or FALSE)
+    console.log("inside onSongSelect");
+    console.log(item);
+
+    const response = await fetch(`http://localhost:3001/narrowsearch/${item.id}`);
+
+    const output = await response.json();
+    
+    this.setState({
+      isViral: output,
+      song: item
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <nav className="">
-          <div className="navbar">
+        <nav className="mx-auto">
+          <div className="navbar justify-content-center">
             {/* <h1 className="">Tiktok Viral Checker ✅</h1> */}
             <h1 className="">Tiktok Viral Checker ✅</h1>
           </div>
         </nav>
-        <div className="" style={{ width: "800px" }}>
-          <h3>See if your song is viral on Tiktok</h3>
-          <form className="" onSubmit={this.onFormSubmit}>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value })}
-            />
-            {this.state.hasSearched && this.state.hasSubmitted ? (
-              <p>
-                Show results for <em>{this.state.term}</em>
-              </p>
-            ) : (
-              <div></div>
-            )}
-          </form>
+        <div className="">
+          <div className="">
+            <h3>See if your song is viral on Tiktok</h3>
+            <form className="" onSubmit={this.onFormSubmit}>
+              <input
+                type="text"
+                value={this.state.term}
+                onChange={(e) => this.setState({ term: e.target.value })}
+              />
+              {this.state.hasSearched && this.state.hasSubmitted ? (
+                <p>
+                  Show results for <em>{this.state.term}</em>
+                </p>
+              ) : (
+                <div></div>
+              )}
+            </form>
+          </div>
         </div>
         <div className="content">
-          {this.state.hasSearched ? (
-            <h1>Is the song viral? {this.state.isViral ? "yes" : "no"}</h1>
+          {this.state.isViral != null ? (
+            <div>
+              <h1>Is "{this.state.song.name}" viral?</h1>
+              <h1>{this.state.isViral ? "Yes" : "No"}</h1>
+            </div>
           ) : (
             <div></div>
           )}
-          <h4>Not on Tiktok?</h4>
-          <p>
-            Use this tool to search a song to see if it's famous from Tiktok.
-          </p>
           <div>
-            Content: {Object.keys(this.state.data).length === 0 ? "empty" : this.renderSearch()}
+            {Object.keys(this.state.data).length === 0
+              ? ""
+              : this.renderSearch()}
           </div>
         </div>
       </div>
